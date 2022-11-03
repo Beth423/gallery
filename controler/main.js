27737f38e049f908be3470493bf48812aa426bbf
Beth423 <59936151+Beth423@users.noreply.github.com>
2022-11-03 11:38:05 -0700

    Create main.js

---
+++ controler/main.js
@@ -0,0 +1,7 @@
+let main =document.getElementById("main")
+let h1 = document.createElement("H1")
+h1.innerText=("hi")
+main.appendChild(h1)
+console.log("here")
+// link to the local host 
+ //𝕙𝕥𝕥𝕡://𝟙𝟚𝟟.𝟘.𝟘.𝟙:𝟝𝟝𝟘𝟘/𝕚𝕟𝕕𝕖𝕩.𝕙𝕥𝕞𝕝
