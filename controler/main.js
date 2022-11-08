27a978e0694469675bcd7e2507b610dfbb79015c
Beth423 <59936151+Beth423@users.noreply.github.com>
2022-11-08 09:52:01 -0800

    Update main.js
    cards loop

--- controler/main.js
+++ controler/main.js
@@ -1,7 +1,21 @@
 let main =document.getElementById("main")
+main.addEventListener("onload",Cards())
 let h1 = document.createElement("H1")
-h1.innerText=("hi")
-main.appendChild(h1)
-console.log("here")
+// h1.innerText=("main content here")
+// main.appendChild(h1)
 // link to the local host 
  //𝕙𝕥𝕥𝕡://𝟙𝟚𝟟.𝟘.𝟘.𝟙:𝟝𝟝𝟘𝟘/𝕚𝕟𝕕𝕖𝕩.𝕙𝕥𝕞𝕝
+ function Cards(){
+    console.log("here")
+
+
+    for (i=0;i<=3 ;i++){
+        let main =document.getElementById("main")
+        let div =document.createElement("Div")
+        div.setAttribute("class","cards")
+        div.setAttribute("id","card_"+i)
+        div.innerHTML="<h2>here</H2>"
+
+        main.appendChild(div)
+    }
+ }
