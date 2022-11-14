24fd2da3bfdf40d388b7a8265f5b41337fbd61ac
Beth423 <59936151+Beth423@users.noreply.github.com>
2022-11-14 11:17:34 -0800

    Update main.js

--- controler/main.js
+++ controler/main.js
@@ -8,15 +8,22 @@ let h1 = document.createElement("H1")
  function Cards(){
     console.log("here")
 
-
-    for (i=0;i<=2 ;i++){
-        let main =document.getElementById("main")
-        let div =document.createElement("Div")
+    let imgLinks=["pillers.jpg","Carina-Nebula.webp","heic0515a.jpg","Trifid_Nebula_by_Deddy_Dayag.jpg"]
+    for (i=0;i<=3 ;i++){
+        let main=document.getElementById("main")
+        let div=document.createElement("Div")
+        let img=document.createElement("img")
+        let captions= document.createElement("div")
         div.setAttribute("class","cards")
         div.setAttribute("id","card_"+i)
-        div.setAttribute("style","transform:rotate("+i*10+"deg);")
-        div.innerHTML="<h2>here</H2>"
+        div.setAttribute("style","transform:rotate("+i*5+"deg); left:"+10 *i/4+"rem;")
+        
+        img.setAttribute("src","/assets/"+imgLinks[i]+"")
+        img.setAttribute("class","img")
 
+        captions.innerHTML="<h1 class ='captions'>"+"here"+"</h1>";
+        div.appendChild(img)
+        div.appendChild(captions)
         main.appendChild(div)
     }
  }
