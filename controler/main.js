2195beffc3c2ee5e0354c0d1b4b3fd838c3f3f86
Beth423 <59936151+Beth423@users.noreply.github.com>
2022-11-14 11:51:35 -0800

    Update main.js

--- controler/main.js
+++ controler/main.js
@@ -21,7 +21,7 @@ let h1 = document.createElement("H1")
         img.setAttribute("src","/assets/images/"+imgLinks[i]+"")
         img.setAttribute("class","img")
 
-        captions.innerHTML="<h1 class ='captions'>"+"here"+"</h1>";
+        captions.innerHTML="<p class='captions'>"+"    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur = "+"</p>";
         div.appendChild(img)
         div.appendChild(captions)
         main.appendChild(div)
