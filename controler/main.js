aca5d95edbfd25093516a535e170821211858fce
Beth423 <59936151+Beth423@users.noreply.github.com>
2022-11-22 08:12:50 -0800

    Update main.js

--- controler/main.js
+++ controler/main.js
@@ -8,15 +8,22 @@ let h1 = document.createElement("H1")
  function Cards(){
     console.log("here")
 
-
+    let imgLinks=["pillers.jpg","Carina-Nebula.webp","heic0515a.jpg","Trifid_Nebula_by_Deddy_Dayag.jpg"]
     for (i=0;i<=3 ;i++){
-        let main =document.getElementById("main")
-        let div =document.createElement("Div")
+        let main=document.getElementById("main")
+        let div=document.createElement("Div")
+        let img=document.createElement("img")
+        let captions= document.createElement("div")
         div.setAttribute("class","cards")
         div.setAttribute("id","card_"+i)
-        div.setAttribute("style","left:"+40*i+"px; rotation:4px;")
-        div.innerHTML="<h2>here</H2>"
+        div.setAttribute("style","transform:rotate("+i*5+"deg); left:"+10 *i/4+"rem;")
+        
+        img.setAttribute("src","/assets/images/"+imgLinks[i]+"")
+        img.setAttribute("class","img")
 
+        captions.innerHTML="<p class='captions'>"+"    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur = "+"</p>";
+        div.appendChild(img)
+        div.appendChild(captions)
         main.appendChild(div)
     }
  }
